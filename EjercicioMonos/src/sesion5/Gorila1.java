f94ab26eca1eb5dadcb0e72853357df0e34847b7
JaviKunSnow <javierandres_11@hotmail.es>
2023-03-20 14:56:24 +0100

    Update Gorila1.java

--- EjercicioMonos/src/sesion5/Gorila1.java
+++ EjercicioMonos/src/sesion5/Gorila1.java
@@ -2,7 +2,7 @@ package sesion5;
 
 import java.util.ArrayList;
 
-public class Gorila1 extends Grupo1{
+public class Gorila1 extends Grupo1 implements Robable, Escondible{
 
 	public Gorila1(String nombre, ArrayList<Comida> almacen) {
 		super(nombre, almacen);
